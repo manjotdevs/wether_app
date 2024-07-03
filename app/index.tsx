732ca860c286b2bb4111bad37fa32de5aae4862e
manjotdevs <161864466+manjotdevs@users.noreply.github.com>
2024-07-03 17:00:02 +0530

    dezining app

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { View, SafeAreaView, Text } from 'react-native';
-import { styled } from 'nativewind';
+import React from "react";
+import { View, SafeAreaView, Text } from "react-native";
+import { styled } from "nativewind";
 
 export const SView = styled(View);
 export const SText = styled(Text);
+export const SSafeAreaView = styled(SafeAreaView);
 
 export default function App() {
   return (
-  <SView>
-    <SText>hi</SText>
-    <SText className="text-red-500">hi</SText>
-  </SView>
+    <SSafeAreaView className="bg-blue-600 h-full ">
+      <SView className="bg-blue-600 ">
+        <SText>hi</SText>
+        <SText className="bg-red-500">hi</SText>
+      </SView>
+    </SSafeAreaView>
   );
-}import React from 'react'
-import {}
+}
